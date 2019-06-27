9d7945059371b7141c81add5336d23fa62c321d0
Teito <s_genz@stud.hwr-berlin.de>
2019-06-27 23:18:51 +0200

    Setup bundle module

--- dcmcbot-bundle/src/assemble/resources/bin/dcmcbot-start.bat
+++ dcmcbot-bundle/src/assemble/resources/bin/dcmcbot-start.bat
@@ -1,1 +1,3 @@
-# script to start dcmcbot application
+@echo off
+set APP_DIR=%~dp0..
+java -jar -DconfigurationFile=%APP_DIR%/conf/dcmcbot-config.xml -Djava.library.path=%APP_DIR%/lib %APP_DIR%/lib/dcmcbot-core-localbuild.jar

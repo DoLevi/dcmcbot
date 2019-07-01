8648312c8354594ad8f3c4785b7972aedb46d09e
Teito <s_genz@stud.hwr-berlin.de>
2019-07-01 11:17:51 +0200

    Made project buildable frmo the get-go

--- dcmcbot-bundle/src/assemble/resources/bin/dcmcbot-start.bat
+++ dcmcbot-bundle/src/assemble/resources/bin/dcmcbot-start.bat
@@ -1,3 +1,4 @@
 @echo off
 set APP_DIR=%~dp0..
-java -jar -DconfigurationFile=%APP_DIR%/conf/dcmcbot-config.xml -Djava.library.path=%APP_DIR%/lib %APP_DIR%/lib/dcmcbot-core-localbuild.jar
+cd %APP_DIR%
+java -jar -Djava.class.path=lib/* -Dlog4j.configurationFile=conf/log4j2.xml -Ddcmcbot.configurationFile=conf\dcmcbot-config.xml lib\dcmcbot-core-localbuild.jar

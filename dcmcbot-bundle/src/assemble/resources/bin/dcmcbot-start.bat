3aa0047210c5ab981468947bb645235d17c2ef31
Teito <s_genz@stud.hwr-berlin.de>
2019-07-07 21:09:29 +0200

    Configs changed for better app/system interactions

--- dcmcbot-bundle/src/assemble/resources/bin/dcmcbot-start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set APP_DIR=%~dp0..
-cd %APP_DIR%
-java -jar -Djava.class.path=lib/* -Dlog4j.configurationFile=conf/log4j2.xml -Ddcmcbot.configurationFile=conf\dcmcbot-config.xml lib\dcmcbot-core-localbuild.jar
